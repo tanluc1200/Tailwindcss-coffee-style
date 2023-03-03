1fce9001156ab95daf099ceaf80eb9b810e71b4e
tanluc <tanluc1200@users.noreply.github.com>
2023-03-03 09:36:58 +0700

    third commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
     content: ["./public/**/*.{html,js}"],
     theme: {
         extend: {
+            fontFamily: {
+                Karla: ["Karla", "sans-serif"],
+            },
             colors: {
                 "light-coffee": "#743f2f;",
             },
